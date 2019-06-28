697b1a7cfc31ce0aa4fd147158d1ac180ff575f3
rathu chandran <46434790+rathu26@users.noreply.github.com>
2019-06-28 12:16:14 +0530

    Create count_charno.c

---
+++ count_charno.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() 
+{
+	char str[500];
+	int count=0,i=0;
+    scanf("%s", str);
+	while(str[i])
+	{
+	    count++;
+	    if(str[i]!=str[i+1])
+	    {
+	        printf("%d%c",count,str[i]);
+	        count=0;
+	    }
+	    i++;
+	}
+	return 0;
+}
