b347732aa24a6d982c5cd7ce5a7e9d591c82ced9
rathu chandran <46434790+rathu26@users.noreply.github.com>
2019-06-28 12:22:28 +0530

    Update count_charno.c

--- count_charno.c
+++ count_charno.c
@@ -17,3 +17,26 @@ int main()
 	}
 	return 0;
 }
+
+
+//decode
+
+#include <stdio.h>
+
+int main() 
+{
+	char str[500];
+	int count=0,i=0;
+    scanf("%s", str);
+	while(str[i])
+	{
+	    count++;
+	    if(str[i]!=str[i+1])
+	    {
+	        printf("%d%c",count,str[i]);
+	        count=0;
+	    }
+	    i++;
+	}
+	return 0;
+}
